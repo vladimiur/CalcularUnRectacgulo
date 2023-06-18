b30e0eb93c3d87a0dfd9e4c4b3064d12244d1853
kevin <Elmer@DESKTOP-VN1F8HC>
2023-06-18 20:35:06 +0200

    Agregar archivos de proyecto.

---
+++ CalcularUnRectacgulo/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace CalcularUnRectacgulo;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	//Creamos dos variables y las multiplicamos
+    private void calcular_Clicked(object sender, EventArgs e)
+    {
+		var ValorB = Convert.ToInt32(basen.Text);
+		var ValorA = Convert.ToInt32(altura.Text);
+		double cal = ValorB * ValorA;
+		respuesta.Text = Convert.ToString(cal);
+    }
+}
+
